7a1ed5ba80ec680f5b640fa64da2529ec4cddea9
Darcy Shen <sadhen@zoho.com>
2017-10-26 03:09:04 +0800

    use ts

---
+++ texmacs/ancient.ts
@@ -0,0 +1,36 @@
+<TeXmacs|1.99.5>
+
+<style|source>
+
+<\body>
+  <\active*>
+    <\src-title>
+      <src-style-file|60-macros|1.0>
+
+      <src-purpose|Automatically generated style file.>
+    </src-title>
+  </active*>
+
+  <use-package|article|chinese>
+
+  <\active*>
+    <\src-comment>
+      Style parameters.
+    </src-comment>
+  </active*>
+
+  <assign|font|Source Han Sans SC>
+
+  <assign|page-medium|paper>
+
+  <\active*>
+    <\src-comment>
+      Macro definitions.
+    </src-comment>
+  </active*>
+
+  <assign|person|<macro|body|<underline|<arg|body>>>>
+</body>
+
+<initial|<\collection>
+</collection>>
